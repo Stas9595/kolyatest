d296bb0532a447ebde23f7e61e9619f7c896211f
stasg <sgrigorevskiy@eccentex.com>
2022-03-20 22:51:09 +0200

    test

--- app.js
+++ app.js
@@ -2,10 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
-app.listen(3000, () => {
-    console.log("Application started and Listening on port 3000");
-});
-
 // server css as static
 app.use(express.static(__dirname));
 
@@ -15,3 +11,5 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
+
+module.exports = app;
